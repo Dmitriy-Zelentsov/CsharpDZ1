41389554a3ed55ef573a5ae1f494d46ba7e545a8
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-05 14:41:54 +0300

    add Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+  int n = 1;
+  Console.WriteLine("Введите конец диапазона N");
+  int N = int.Parse(Console.ReadLine());
+  for (int i = n; i <= N; i++)
+  {
+    if (i % 2 == 0)
+     Console.Write(i + " ");
+     }
+
